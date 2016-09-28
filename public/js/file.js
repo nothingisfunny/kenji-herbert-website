92f16ef2553384dcaa124031ffc921b9d592ad2e
nothingisfunny <if.the.kids.r.united@gmail.com>
2016-09-27 19:43:09 -0700

    events with hidden past events

--- public/js/file.js
+++ public/js/file.js
@@ -8,5 +8,5 @@ document.addEventListener('DOMContentLoaded', function() {
 	        this.nextElementSibling.classList.toggle("show");
 	    }
 	}
-})
+} )
 /* Toggle between adding and removing the "active" and "show" classes when the user clicks on one of the "Section" buttons. The "active" class is used to add a background color to the current button when its belonging panel is open. The "show" class is used to open the specific accordion panel */
